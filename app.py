cc0e0842188206b3763eadc10cbf26f9f1526362
royba23-meet <roybacharschool@gmail.com>
2022-08-09 17:58:15 +0300

    WORKS WITH DB | Need to add homepage then finished with everything

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 import os
 from werkzeug.datastructures import ImmutableMultiDict
 from flask import Flask, flash, jsonify, render_template, redirect, request, session, url_for
-import xmltodict
+import pyrebase
 
+
+CONFIG = { 
+  "apiKey": "AIzaSyB_CpKIs0IjxutT_4HHpS-taOBRYVRahK8",
+  "authDomain": "case-study-8d85f.firebaseapp.com",
+  "projectId": "case-study-8d85f",
+  "storageBucket": "case-study-8d85f.appspot.com",
+  "messagingSenderId": "679538380741",
+  "appId": "1:679538380741:web:0746894f84c609f8dac5ac",
+  "measurementId": "G-LRSHRELNCV",
+  "databaseURL" : "https://case-study-8d85f-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+firebase = pyrebase.initialize_app(CONFIG)
+db = firebase.database()
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        name = request.form.get('name')
-        surname = request.form.get('surname')
-        message = request.form.get('message')
-        print(name, surname, message)
+        db.child("messages").push({"name": request.json['name'], "surname": request.json['surname'], "message": request.json['message']})
         return render_template('index.html')
     return render_template('index.html')
 
